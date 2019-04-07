e52ef912e5f40f5e5170bd40cbfeefec158fa1ba
Gustavo Gard <gusgard@gmail.com>
2019-04-07 19:50:30 -0300

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from "react-responsive-carousel"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+import example from "../images/gatsby-astronaut.png"
 
+const keywords = ["edificio", "edificios", "uruguay", "montevideo"]
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={keywords} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Carousel infiniteLoop autoPlay swipeable={true}>
+      <div>
+        <img src={example} alt="example" />
+        <p className="legend">example1</p>
+      </div>
+      <div>
+        <img src={example} alt="example" />
+        <p className="legend">example1</p>
+      </div>
+      <div>
+        <img src={example} alt="example" />
+        <p className="legend">example1</p>
+      </div>
+    </Carousel>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
